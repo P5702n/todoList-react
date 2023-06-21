88cee4826176cac85081dc3bee62e067931bbf1f
P5702n <pradipnagdive@gmail.com>
2023-06-21 15:39:47 +0530

    modified:   src/components/AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -26,13 +26,13 @@ const AddTodo = (props) => {
                 <h3 style={{ color: props.modeStyle === "dark" ? "white" : "black" }}>Add todo here.</h3>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlInput1" style={{ color: props.modeStyle === "dark" ? "white" : "black" }} className="form-label">Enter Todo title</label>
-                    <input type="text" className="form-control" style={{ color: props.modeStyle === "dark" ? "black" : "white", background: props.modeStyle === "dark" ? "teal" : "rgb(220, 220, 248)" }} id="exampleFormControlInput1" onChange={(e) => {
+                    <input type="text" className="form-control" style={{ color: props.modeStyle === "dark" ? "white" : "black", background: props.modeStyle === "dark" ? "teal" : "rgb(220, 220, 248)" }} id="exampleFormControlInput1" onChange={(e) => {
                         setTitle(e.target.value);
                     }} value={title} key={props.todos.length + 1} placeholder="Todo title here." />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleFormControlTextarea1" style={{ color: props.modeStyle === "dark" ? "white" : "black" }} className="form-label">Your Todo description here.</label>
-                    <textarea className="form-control" style={{ color: props.modeStyle === "dark" ? "black" : "white", background: props.modeStyle === "dark" ? "teal" : "rgb(220, 220, 248)" }} id="exampleFormControlTextarea1" value={desc} key={props.todos.srn + 1} onChange={(e) => {
+                    <textarea className="form-control" style={{ color: props.modeStyle === "dark" ? "white" : "black", background: props.modeStyle === "dark" ? "teal" : "rgb(220, 220, 248)" }} id="exampleFormControlTextarea1" value={desc} key={props.todos.srn + 1} onChange={(e) => {
                         setDesc(e.target.value);
                     }} rows="4"></textarea>
                 </div>
